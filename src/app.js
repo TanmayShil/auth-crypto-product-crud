4989e08a207f91193ef3683efc15f6201955a004
TanmayShil <tanmayshil2002@gmail.com>
2026-09-23 15:21:02 +0530

    okay 3

--- src/app.js
+++ src/app.js
@@ -1,49 +1,21 @@
 const express = require("express");
 const cors = require("cors");
-const cookieParser = require("cookie-parser");
 
-const routes = require("./routes/index");
 const app = express();
 
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "http://localhost:5174",
-      "http://localhost:3000",
-      "http://localhost:3001",
-      "http://localhost:6899",
-    ],
-    credentials: true,
+    origin: "*",
   }),
 );
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.status(200).json({
     success: true,
-    message: "Role Based Authentication System API is running",
-  });
-});
-
-app.use("/api", routes);
-
-app.use((req, res) => {
-  res.status(404).json({
-    success: false,
-    message: "Route not found",
-  });
-});
-
-app.use((error, req, res, next) => {
-  logger.error(`Unhandled server error: ${error.message}`);
-
-  return res.status(500).json({
-    success: false,
-    message: "Internal server error",
+    message: "Vercel Express API is working",
   });
 });
 
