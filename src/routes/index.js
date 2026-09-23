5a25f9c24f779b0f2b1da0f08f910b3e761c45ac
TanmayShil <tanmayshil2002@gmail.com>
2026-09-23 15:39:18 +0530

    dummy routes added

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,27 @@
+// const express = require("express");
+// const authRoutes = require("./authRoutes");
+// const userRoutes = require("./userRoutes");
+// const categoryRoutes = require("./categoryRoutes");
+// const productRoutes = require("./productRoutes");
+
+// const router = express.Router();
+
+// router.use("/auth", authRoutes);
+// router.use("/admin", userRoutes);
+// router.use("/", categoryRoutes);
+// router.use("/", productRoutes);
+
+// module.exports = router;
+
 const express = require("express");
-const authRoutes = require("./authRoutes");
-const userRoutes = require("./userRoutes");
-const categoryRoutes = require("./categoryRoutes");
-const productRoutes = require("./productRoutes");
 
 const router = express.Router();
 
-router.use("/auth", authRoutes);
-router.use("/admin", userRoutes);
-router.use("/", categoryRoutes);
-router.use("/", productRoutes);
+router.get("/test", (req, res) => {
+  res.json({
+    success: true,
+    message: "Routes are working",
+  });
+});
 
 module.exports = router;
